7f9e1ecf4417900e356e3794e5c01dcfab913021
mbscale <bapna_mukul00@yahoo.com>
2021-03-03 14:11:38 +0000

    add k8

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -5,14 +5,22 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="mukulbapna/udacity-prediction"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+docker login
+kubectl create deployment prediction-as-a-pod --image=$dockerpath:latest
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get deployments
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward deployment/prediction-as-a-pod 8000:80
+
 
